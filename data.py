7b32e5469adf05d555bcf7604de7d5c42b211e92
Boris Shminke <boris@shminke.ml>
2021-07-08 16:32:13 +0200

    tidy class structure

--- data.py
+++ data.py
@@ -1,32 +1,34 @@
-import torch
-from torch.utils.data import IterableDataset
-from tptp_graph import graph_of
 from glob import glob
 from pathlib import Path
 from random import shuffle
 
-def problems(blacklist, randomise=True):
-    paths = glob('Problems/**/*+*.p')
-    if randomise:
-        shuffle(paths)
-    for path in paths:
-        if path in blacklist:
-            continue
-        nodes, sources, targets = graph_of(path)
-        if nodes.shape[0] > 50000:
-            blacklist.add(path)
-            continue
-        nodes = torch.tensor(nodes)
-        sources = torch.tensor(sources)
-        targets = torch.tensor(targets)
-        path = Path(path)
-        problem = path.stem
-        yield problem, nodes, sources, targets
+import torch
+from torch.utils.data import IterableDataset
+from tptp_graph import graph_of
+
 
 class Problems(IterableDataset):
-    def __init__(self):
+    def __init__(self, paths = glob('Problems/**/*+*.p'), randomise=True):
         self.blacklist = set()
-        super().__init__()
+        self.paths = paths
+        self.randomise = randomise
 
     def __iter__(self):
-        return problems(self.blacklist)
+        if self.randomise:
+            shuffle(self.paths)
+        for path in self.paths:
+            if path in self.blacklist:
+                continue
+            nodes, sources, targets = graph_of(path)
+            if nodes.shape[0] > 50000:
+                self.blacklist.add(path)
+                continue
+            nodes = torch.tensor(nodes)
+            sources = torch.tensor(sources)
+            targets = torch.tensor(targets)
+            path = Path(path)
+            problem = path.stem
+            yield problem, nodes, sources, targets
+
+    def __len__(self):
+        return len(self.paths)

d1bba2b4ab41e23d5754c44f796c5337ae6b6568
Boris Shminke <boris@shminke.ml>
2021-07-09 08:37:21 +0200

    use Problem class instead of function

--- embed.py
+++ embed.py
@@ -1,23 +1,22 @@
-import torch
-from pathlib import Path
 import sys
+from pathlib import Path
 
+import torch
+
+from data import Problems
 from model import Model
-from data import problems
 
-if __name__ == '__main__':
-    model = Model().to('cuda')
+if __name__ == "__main__":
+    model = Model().to("cuda")
     model.eval()
-    model.load_state_dict(torch.load('model.pt'))
+    model.load_state_dict(torch.load("model.pt"))
 
     print("problem\tdomain", file=sys.stderr)
-    for problem, nodes, sources, targets in problems(set(), randomise=False):
+    for problem, nodes, sources, targets in Problems():
         with torch.no_grad():
-            name = Path(problem).stem 
+            name = Path(problem).stem
             print(name + "\t" + name[:3], file=sys.stderr)
             embedding = model.encode(
-                nodes.to('cuda'),
-                sources.to('cuda'),
-                targets.to('cuda')
+                nodes.to("cuda"), sources.to("cuda"), targets.to("cuda")
             )
             print("\t".join(str(float(x)) for x in embedding))
